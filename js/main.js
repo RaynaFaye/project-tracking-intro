e715f9bcd6342c6d74fbd48d2689ee412a6d76fb
Serena <serenafayelombardo@gmail.com>
2020-06-12 11:37:48 +0200

    Added the accessibility aria controls change to the JS

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ menuButton.addEventListener('click', () => {
   if (menuBlock.classList.contains('header__menu__hidden')) {
     menuBlock.classList.remove('header__menu__hidden');
     menuButtonImage.src = 'images/icon-close.svg';
+    menuButton.setAttribute('aria-expanded', 'true');
   } else {
     menuBlock.classList.add('header__menu__hidden');
     menuButtonImage.src = 'images/icon-hamburger.svg';
+    menuButton.setAttribute('aria-expanded', 'false');
   }
 });
