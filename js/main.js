76355c6c80ae1045b975126de77498bdf0f5ccb3
Serena <serenafayelombardo@gmail.com>
2020-06-12 11:35:07 +0200

    Added functionality to open and close menu on mobile

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const menuButton = document.querySelector('.header__mobile-menu__button');
+
+menuButton.addEventListener('click', () => {
+  const menuButtonImage = menuButton.querySelector('img');
+  const menuBlock = document.querySelector('.header__menu');
+  if (menuBlock.classList.contains('header__menu__hidden')) {
+    menuBlock.classList.remove('header__menu__hidden');
+    menuButtonImage.src = 'images/icon-close.svg';
+  } else {
+    menuBlock.classList.add('header__menu__hidden');
+    menuButtonImage.src = 'images/icon-hamburger.svg';
+  }
+});
